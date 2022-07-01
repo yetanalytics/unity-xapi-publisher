982365db0d4314d7236040c49a7b087429ef4c2f
Henk Reder <henk@yetanalytics.com>
2022-07-01 07:39:58 -0400

    added sendoff methods

--- Assets/xApiIntegration.cs
+++ Assets/xApiIntegration.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
+using System;
 using UnityEngine;
 using RestSharp;
 using RestSharp.Authenticators;
@@ -10,26 +11,53 @@ using Domain;
 
 public class xApiIntegration : MonoBehaviour
 {
+    // LRS Credentials
+    public string lrsUrl;
+    public string lrsKey;
+    public string lrsSecret;
+
+    // Metadata Variables
+    public string email;
+    public string uname;
+    public string gameId;
+    public string gameName;
+
+    private Creator creator{get {return new Creator();}}
+    private Sender sender{get {return new Sender(lrsUrl,lrsKey,lrsSecret);}}
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        CreateStatement();
+    void Start() {
+        SendStartedStatement();
     }
 
-    async void CreateStatement() {
-        var creator = new Create();
-        var statement = await creator.StartedStatement("mailto:user@example.com",
-                                                       "John Doe",
-                                                       "http://video.gms/pac-man",
-                                                       "Pac-Man");
-        var sender = new Sender("http://localhost:8080",
-                                "username",
-                                "password");
+    public void OnButtonPress() {
+        SendCompletedStatement();
+    }
+
+    async void SendStartedStatement() {
+        var statement = await creator.StartedStatement(String.Format("mailto:{0}",email),
+                                                       uname,
+                                                       gameId,
+                                                       gameName);
         var statementStr = statement.Serialize();
         var response = await sender.SendStatement(statementStr);
         print(statementStr);
         print(response.Content);
         print(response.ResponseStatus);
+    }
+
+    async void SendCompletedStatement() {
+        var statement = await creator.CompletedStatement(String.Format("mailto:{0}",email),
+                                                         uname,
+                                                         gameId,
+                                                         gameName);
+        var statementStr = statement.Serialize();
+        var response = await sender.SendStatement(statementStr);
+        print(statementStr);
+        print(response.Content);
+        print(response.ResponseStatus);
+
     }
 
     // Update is called once per frame
