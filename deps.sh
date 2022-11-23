37d589b762a5f7063e2427c50ce75980434d043e
Henk Reder <henk@yetanalytics.com>
2022-11-23 15:27:39 -0500

    added System.Memory and System.Threading.Tasks.Extensions

--- deps.sh
+++ deps.sh
@@ -1,6 +1,6 @@
-#/bin/sh
+#!/bin/bash
 
-nuget restore packages.config
+nuget restore packages.config -PackagesDirectory Packages
 mkdir Assemblies
 cp Packages/*/lib/netstandard2.0/*.dll Assemblies
 rm -rf Packages
